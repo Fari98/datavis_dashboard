d1f7212c62716b509a81bf77a27276e8d23db46d
Fari98 <71496424+Fari98@users.noreply.github.com>
2021-03-31 12:15:08 +0100

    change app version

--- loasangeles-crime.py
+++ loasangeles-crime.py
@@ -12,6 +12,7 @@ victim_donut_data = pd.read_csv('data/victims_donut_data.csv', index_col = 0)
 pop_area_count = pd.read_csv('data/pop_area_count.csv', index_col = 0)
 crime_bar = pd.read_csv('data/la_crime_data.csv')
 crime_type = pd.read_csv('data/crime_type.csv', index_col = 0)
+crimeAreaDF = pd.read_csv('data/crimeDate_crimeTypes.csv', index_col = 0)
 pop_data = pd.read_csv('data/Population_HV.csv')
 
 crime_type['Date'] = pd.to_datetime(crime_type.Date)
@@ -78,7 +79,7 @@ def most_affected_area(data, year):
         mode = "number",
         value = crimes_occured_most,
         number={"font":{"size":40}},
-        title = {"text": f"Most Affected Area<br><br><span style='font-size:1.8em;color:gray'>{most_affected_area}</span>"}
+        title = {"text": f"Area with the highest number<br>of crimes in {year_selected}<br><br><span style='font-size:1.8em;color:gray'>{most_affected_area}</span>"}
     ))
     
     return fig        
@@ -124,7 +125,7 @@ def least_affected_area(data, year):
         mode = "number",
         value = crimes_occured_most,
         number={"font":{"size":40}},
-        title = {"text": f"Most Affected Area<br><br><span style='font-size:1.8em;color:gray'>{most_affected_area}</span>"}
+        title = {"text": f"Area with the lowest number<br>of crimes in {year_selected}<br><br><span style='font-size:1.8em;color:gray'>{most_affected_area}</span>"}
     ))
     
     return fig  
@@ -145,7 +146,7 @@ def most_affected_year(data, area):
         mode = "number",
         value = int(crimes_occured_most_year),
         number={"font":{"size":40}},
-        title = {"text": f"Most Affected Year<br><br><span style='font-size:1.8em;color:gray'>{most_affected_year}</span>"}
+        title = {"text": f"Year with highest number<br> of crimes in {area_selected}<br><br><span style='font-size:1.8em;color:gray'>{most_affected_year}</span>"}
         ))
     
     return fig
@@ -175,35 +176,49 @@ def barchart(year):
     crime_sel = crime_type.groupby('Year').sum().query('Year == @year').T
     
     data = dict(type='bar', x= crime_sel[year].sort_values(), y=crime_sel.index, orientation = 'h')
-    layout = dict( xaxis = dict(title = 'Count')) #yaxis=dict(title='Crime Type'),
+    layout = dict( xaxis = dict(title = 'Count'))#, yaxis=dict(ticklabelposition = "inside right"))
     
     fig = go.Figure(data=data, layout=layout)
+
+    fig.update_layout(dict(yaxis = dict(ticklabelposition = "inside right")))
     
+    fig.update_layout(autosize=False, width=2000, height=600)
+
     return fig
 
-def crime_line(area):
+def crime_line(year, area):
     
-    crime_type_Area = crime_type.query('Area == @area')
-    crime_type_Area_2 = pd.DataFrame()
-    crime_type_Area_2['crime_sum'] = crime_type_Area[crime_type_Area.columns[2:-1]].sum(axis = 1)
-    crime_type_Area_2['Date'] = crime_type_Area['Date']
+    crime_type_Area = crime_type.query('Area == @area and Date.dt.year == @year')
+    # crime_type_Area = crime_type_Area[crime_type_Area['Date'].str.contains(year)]
+    crimes_list = ['Agravated assault', 'Burglary','Burglary from vehicle', 'Intimate partner assault', 'Simple assault',
+                   'Small theft (under 950$)', 'Stolen vehicle', 'Teft of identity','Vandalism (felony)', 
+                   'Vandalism (misdeameanor)']
+    crimes_to_hide = ['Agravated assault', 'Intimate partner assault', 'Teft of identity', 'Vandalism (felony)', 'Vandalism (misdeameanor)']
     
-    data = dict(type = 'scatter',
-                x = crime_type_Area_2['Date'],
-                y = crime_type_Area_2.crime_sum)
-    
+    data = [dict(type = 'scatter',
+                x = crime_type_Area['Date'],
+                y = crime_type_Area[crime],
+                name=crime)for crime in crimes_list]
+    for d in data:
+        if d.get('name') in crimes_to_hide:
+            d['visible'] = 'legendonly'    
 
-    layout = dict(yaxis = dict(title = 'Count'), xaxis = dict(title = 'Year'))
+    layout = dict(yaxis = dict(title = 'Count'), xaxis = dict(title = 'Date'))
     fig = go.Figure(data=data, layout=layout)
     return fig
 
 ## Layout
 
 st.title('Crime in Los Angeles')
+st.subheader('Visual  interactive analysis of Los Angeles crimes from 2010 though 2019')
+
+st.write('')
+st.write('')
+st.write('')
 
 ## Sidebar
 
-year_selected= st.slider("Select year", min(crime.Year), max(crime.Year))
+year_selected= st.slider("Select a year", min(crime.Year), max(crime.Year))
 crime = crime.query("Year == @year_selected")
 
 ## First Row
@@ -231,7 +246,7 @@ midpoint = (np.average(crime["LAT"]), np.average(crime["LON"]))
 barchart = barchart(year_selected)
 with left2:
 
-    st.header("Los Angeles crimes in %i" % (year_selected))
+    st.header("Map of Los Angeles crimes in %i" % (year_selected))
     st.write('')
     map(crime, midpoint[0], midpoint[1], 8.5)
 
@@ -239,10 +254,7 @@ with right2:
     st.header("Top 10 most occurent cryme types in %i" % (year_selected))
     st.plotly_chart(barchart, use_container_width = True)
     
-
-st.write('')
-st.write('')
-st.write('')
+# Expander Selector
 
 areas = ['Newton',
  'Pacific',
@@ -280,16 +292,12 @@ ma_year = most_affected_year(crime_bar, area_selected)
 pop_perc = population_percentage(pop_data, area_selected)
 with left3:
     st.plotly_chart(ma_year, use_container_width = True)
-    st.plotly_chart(pop_perc, use_container_width = True)
+    # st.plotly_chart(pop_perc, use_container_width = True)
 
-lc = crime_line(area_selected)
+lc = crime_line(year_selected, area_selected)
 with right3:
-    st.header(f'Top 10 most occurent cryme types in {area_selected} and their evolution through time.')
+    st.header(f'Top 10 most occured cryme types in {area_selected} and their evolution through {year_selected}.')
     st.plotly_chart(lc, use_container_width = True)
-
-st.write('')
-st.write('')
-st.write('')
 
 ## Fourth Row 
 left4, spacer, right4 = st.beta_columns((1, 0.1, 1))
@@ -299,10 +307,12 @@ bar = bar_chart(area_selected)
 
 with left4:
     
+    st.header(f'Crime rate in {area_selected} compared to the Los Angeles average')
     st.plotly_chart(bar, use_container_width= True)
-    st.write(f'Crime rate in {area_selected} compared to the average in Los Angeles')
+    
 
 with right4:
     
+    st.header(f'Ethnicity distribution in {area_selected}')
     st.plotly_chart(ring, use_container_width = True)
-    st.write(f'Ethnicity distribution in {area_selected}')
+    
